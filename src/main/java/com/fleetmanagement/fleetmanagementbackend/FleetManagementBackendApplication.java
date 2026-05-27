70a711a85bf503af37f5b775c40274edaa33b307
roshan7-1 <roshanghogare07@gmail.com>
2026-05-27 20:10:22 +0530

    feat: create driver and vehicle entities

--- src/main/java/com/fleetmanagement/fleetmanagementbackend/FleetManagementBackendApplication.java
+++ src/main/java/com/fleetmanagement/fleetmanagementbackend/FleetManagementBackendApplication.java
@@ -1,4 +1,4 @@
-package com.fleetmanagement.fleet_management_backend;
+package com.fleetmanagement.fleetmanagementbackend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
