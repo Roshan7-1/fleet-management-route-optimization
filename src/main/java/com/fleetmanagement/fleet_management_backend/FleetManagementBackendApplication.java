b37240b9b49528867ae20e9902e83f33baf1ed34
roshan7-1 <roshanghogare07@gmail.com>
2026-05-27 19:04:15 +0530

    chore: initialize spring boot backend project

---
+++ src/main/java/com/fleetmanagement/fleet_management_backend/FleetManagementBackendApplication.java
@@ -0,0 +1,13 @@
+package com.fleetmanagement.fleet_management_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FleetManagementBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FleetManagementBackendApplication.class, args);
+	}
+
+}
